ab3b1267b7e638eec1a36b4d963a3d4501908cdd
Heidi Chen <heidi1105@gmail.com>
2023-03-13 15:59:49 +0000

    quickSort

--- solution/quickSort2.js
+++ solution/quickSort2.js
@@ -4,7 +4,7 @@
         the last idx.
     - later these params will be used to specify a sub section of the array to
         partition.
-  Steps
+  Steps:
   1. Pick an number out of the arr to be your pivot value
     - usually the middle idx but can be any.
   2. Partition the array IN PLACE such that all values less than the pivot
@@ -40,13 +40,15 @@ const nums2 = [11, 8, 14, 3, 3, 3, 6, 2, 7];
 const nums3 = [1, 17, 12, 3, 9, 13, 21, 4, 27];
 const nums4 = [2, 1];
 
+
+
 /**
  * Partitions the given array in-place by selecting the number at the middle
  * index to use it as a "pivot" value, then arranges all numbers less than the
  * pivot to be to it's left and all larger numbers to the right of the pivot.
  * - Time: O(?).
  * - Space: O(?).
- * @see num
+ * @see https://www.hackerearth.com/practice/algorithms/sorting/quick-sort/visualize/
  *    visualization of quickSort. Focus only on the first cycle of the visualization
  *    for the partition portion only.
  * @param {Array<number>} nums
@@ -57,31 +59,6 @@ const nums4 = [2, 1];
  * @returns {Array<number>} The idx where left section of smaller items ends.
  */
 function partition(nums = [], left = 0, right = nums.length - 1) {
-  const pivot = nums[Math.floor(nums.length / 2)]
-  const leftArr = []
-  const rightArr = []
-  for(let i = 0; i < nums.length ; i++){
-    if(i === nums[Math.floor(nums.length / 2)]){
-      continue
-    }
-    if(nums[i] >= pivot){
-      rightArr.push(nums[i])
-    }else{
-      leftArr.push(nums[i])
-    }
-  }
-  leftArr.push(pivot) 
-  console.log(pivot)
-  return leftArr.concat(rightArr)
-  
-}
-
-// console.log(partition(nums1))
-// console.log(partition(nums2))
-// console.log(partition(nums3))
-// console.log(partition(nums4))
-
-function partition2(nums = [], left = 0, right = nums.length - 1) {
   let pivot = nums[Math.floor((left + right) / 2)]
 
 
@@ -104,47 +81,28 @@ function partition2(nums = [], left = 0, right = nums.length - 1) {
   return left;
 }
 
-function partition3(nums, leftIdx = 0, rightIdx = nums.length - 1) {
-  const midIdx = Math.floor((leftIdx + rightIdx) / 2);
-  const pivotVal = nums[midIdx];
-  const tempPivotIdx = rightIdx;
+console.log(nums2)
+console.log(partition(nums2))
+console.log(nums2)
 
-  // Swap the pivot to the end of the section being partitioned so its
-  // position can be kept track of, then move it last to its final position.
-  [nums[midIdx], nums[tempPivotIdx]] = [nums[tempPivotIdx], nums[midIdx]];
 
-  // Skip over the pivot that was moved to the end so it stays there for now.
-  rightIdx = tempPivotIdx - 1;
-
-  // Look for a num on the left and on the right that both need to be moved to
-  // the other side so one swap can move both of them to the correct side.
-  while (true) {
-    // Move leftIdx until we find a num that is out of place.
-    while (nums[leftIdx] < pivotVal) {
-      leftIdx += 1;
-    }
-
-    // Move rightIdx until we find a num that is out of place.
-    while (nums[rightIdx] > pivotVal) {
-      rightIdx -= 1;
-    }
-
-    // All nums have been iterated over, partitioning is complete.
-    if (leftIdx >= rightIdx) {
-      // Swap the pivot to it's final location.
-      [nums[tempPivotIdx], nums[leftIdx]] = [nums[leftIdx], nums[tempPivotIdx]];
-      
-      console.log(nums)
-      console.log("Pivot: "+ pivotVal)
-      
-      return leftIdx;
-    }
-
-    // Swap the two out of place nums so they will both be on the correct side.
-    [nums[leftIdx], nums[rightIdx]] = [nums[rightIdx], nums[leftIdx]];
-
-    // After swapping, we're done with this pair, move on.
-    leftIdx += 1;
-    rightIdx -= 1;
+function quickSort(nums, left = 0, right = nums.length - 1) {
+  let index;
+  if (nums.length > 1) {
+    index = partition(nums, left, right)
+    if (left < index - 1)
+      quickSort(nums, left, index - 1)
+    if (index < right)
+      quickSort(nums, index, right)
   }
+  return nums
 }
+
+
+const test = [9, 1, 1, 6, 10, 1, 1, 1]
+
+console.log("Quicksort")
+console.log(nums2)
+console.log(quickSort(nums2))
+console.log(nums2)
+
