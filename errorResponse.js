ef3f06ac5d2c3d4287558228f004f8b540eebecc
Heidi Chen <heidi1105@gmail.com>
2023-03-21 14:58:43 +0000

    error response

---
+++ errorResponse.js
@@ -0,0 +1,30 @@
+const errorResponseData = { // in .catch(err=> err.response.data.errors)
+        "title": {
+            "name": "ValidatorError",
+            "message": "Title is required",
+            "properties": {
+                "message": "Title is required",
+                "type": "required",
+                "path": "title",
+                "value": ""
+            },
+            "kind": "required",
+            "path": "title",
+            "value": ""
+        },
+        "artist": {
+            "name": "ValidatorError",
+            "message": "Artist is required",
+            "properties": {
+                "message": "Artist is required",
+                "type": "required",
+                "path": "artist",
+                "value": ""
+            },
+            "kind": "required",
+            "path": "artist",
+            "value": ""
+        }
+    }
+
+const errMsgArr =[]
